494fa45cd461d3afe30928f4e83c7970ce469185
EC2 Default User <ec2-user@ip-172-31-72-40.ec2.internal>
2021-12-20 12:42:44 +0000

    docker and kubernetes changes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=sthirupws01/udacity-ml-service
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run sthirupws01 --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods 
+
+#wait until kubernetes pods is ready
+sleep 90s 
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward sthirupws01 8000:80 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=sthirupws01/udacity-ml-service
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username sthirupws01
+docker tag udacity-ml-service $dockerpath
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
